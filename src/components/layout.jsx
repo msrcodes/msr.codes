f8225d73b6762692720dccdc53e7e20162838afe
Mikael <mikaelsrozee@gmail.com>
2020-12-09 19:46:21 +0000

    Added grid layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Layout = ({location, title, children}) => (
-	<>
-		<header>This will be the header</header>
-		<main>{children}</main>
-		<footer>This will be the footer</footer>
-	</>
+	<div className="grid md:grid-rows-layout md:grid-cols-layout grid-rows-layoutm grid-cols-layoutm">
+		<header className="bg-red-300">This will be the header</header>
+		<main className="bg-yellow-300">{children}</main>
+		<footer className="bg-green-300 col-span-full">This will be the footer</footer>
+	</div>
 )
 
 export default Layout
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
 	purge: ['/src/**/*.{js,jsx,ts,tsx}'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateColumns: {
+				layout: '1fr 2fr',
+				layoutm: 'auto',
+			},
+			gridTemplateRows: {
+				layout: 'auto auto',
+				layoutm: 'auto auto auto',
+			},
+		},
 	},
 	variants: {
 		extend: {},
