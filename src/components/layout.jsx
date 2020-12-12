8f72d014fbc33e7670c485eb5c9411c9f6f8e022
Mikael <mikaelsrozee@gmail.com>
2020-12-12 16:16:38 +0000

    Add color scheme

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -6,7 +6,7 @@ import Footer from './Footer'
 const Layout = ({location, title, children}) => (
 	<div className="min-h-screen grid md:grid-rows-layout md:grid-cols-layout grid-rows-layoutm grid-cols-layoutm">
 		<Header />
-		<main className="bg-yellow-300">{children}</main>
+		<main className="bg-secondary dark:bg-secondary-dark">{children}</main>
 		<Footer />
 	</div>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
 	purge: ['/src/**/*.{js,jsx,ts,tsx}'],
-	darkMode: false, // or 'media' or 'class'
+	darkMode: 'media',
 	theme: {
+		colors: {
+			primary: {
+				DEFAULT: '#00aec6',
+			},
+			secondary: {
+				DEFAULT: '#FFFFFF',
+				dark: '#1F2945',
+			},
+		},
 		extend: {
 			gridTemplateColumns: {
 				layout: '1fr 2fr',
