497f82a8590904c45dfdda1cf0de808329eedf0f
Mikael <mikaelsrozee@gmail.com>
2020-12-16 21:42:14 +0000

    Fix build?

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ module.exports = {
 	plugins: [
 		'gatsby-plugin-postcss',
 		'gatsby-plugin-netlify-cms',
-		{
+		/* {
 			resolve: 'gatsby-source-filesystem',
 			options: {
 				path: `${__dirname}/blog`,
 				name: 'markdown-pages',
 			},
-		},
+		}, */
 	],
 }
