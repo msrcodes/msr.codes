0c4fa05eaa0e6c2a09f38d1fb9ea9734c1500fa1
mikaelsrozee <mikaelsrozee@gmail.com>
2020-12-18 21:08:19 +0000

    Get header data from CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,20 @@ module.exports = {
 	plugins: [
 		'gatsby-plugin-postcss',
 		'gatsby-plugin-netlify-cms',
-		/* {
+		'gatsby-transformer-json',
+		{
 			resolve: 'gatsby-source-filesystem',
 			options: {
-				path: `${__dirname}/blog`,
-				name: 'markdown-pages',
+				path: `${__dirname}/_content/blog`,
+				name: 'blogs',
 			},
-		}, */
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				path: `${__dirname}/_content/navigation`,
+				name: 'navigations',
+			},
+		},
 	],
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,39 @@
 import React from 'react'
+import {graphql, Link, useStaticQuery} from 'gatsby'
 
-const Header = () => <header className="bg-primary p-4">This will be the header</header>
+const HeaderLinks = ({className, edges}) => (
+    <>
+        {
+            edges.map(({node: {text, href}}) => (
+                /^\/(?!\/)/.test(href) ? <Link to={href} className={className}>{text}</Link> : <a href={href} className={className}>{text}</a>
+            ))
+        }
+    </>
+)
+
+const Header = () => {
+    const data = useStaticQuery(
+        graphql`
+            query {
+                allNavigationJson {
+                    edges {
+                        node {
+                            text
+                            href
+                        }
+                    }
+                }
+            }
+        `
+    )
+
+    const edges = data?.allNavigationJson?.edges || []
+
+    return (
+        <header className="bg-primary p-4 flex md:flex-col">
+            <HeaderLinks className="hover:underline my-2 mr-2" edges={edges} />
+        </header>
+    )
+}
 
 export default Header
