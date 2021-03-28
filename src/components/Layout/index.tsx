faf52897f3cdb02539b285b41520ff3048327f87
Mikael Rozee <mikaelsrozee@gmail.com>
2021-03-28 19:43:26 +0100

    Create SEO component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,13 +1,20 @@
 import { ReactNode } from 'react'
 
+import SEO from '../SEO'
+import type {SEOProps} from '../SEO'
+
 type LayoutProps = {
     children?: ReactNode,
+    seoProps: SEOProps,
 }
 
-const Layout = ({children}: LayoutProps) => (
-    <main>
-        {children}
-    </main>
+const Layout = ({children, seoProps}: LayoutProps) => (
+    <>
+        <SEO {...seoProps} />
+        <main>
+            {children}
+        </main>
+    </>
 )
 
 export default Layout
---
+++ src/components/SEO/index.tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+
+export type SEOProps = {
+    title: string,
+    description: string,
+    siteName: string,
+    siteUrl: string,
+    twitterUser: string,
+    favicon: string,
+    image: string,
+}
+
+const SEO = ({title, description, siteName, siteUrl, twitterUser, favicon, image}: SEOProps) => (
+    <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:type" content="website" />
+        <meta name="og:title" property="og:title" content={title} />
+        <meta name="og:description" property="og:description" content={description} />
+        <meta property="og:site_name" content={siteName} />
+        <meta property="og:url" content={siteUrl} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:site" content={twitterUser} />
+        <meta name="twitter:creator" content={twitterUser} />
+        <link rel="icon" type="image/png" href={favicon} />
+        <link rel="apple-touch-icon" href={favicon} />
+        <meta property="og:image" content={image} />  
+        <meta name="twitter:image" content={image} />  
+    </Head>
+)
+
+export default SEO
