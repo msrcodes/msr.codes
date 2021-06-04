b734f72667ca5ff1abc6885dd837f94a0419d35f
Mikael Rozee <mikaelsrozee@gmail.com>
2021-06-04 21:35:04 +0100

    enh(header): added roundedness and shadow

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,11 +8,15 @@ interface HeaderProps {
 	links?: HeaderLinkProps[],
 }
 
-const Header: FC<HeaderProps> = ({links = []}) => {
+const Header: FC<HeaderProps> = ({links = [
+	{href: '/', title: 'Blog'},
+	{href: '/', title: 'What I use'},
+	{href: '/', title: 'Portfolio'},
+]}) => {
 	const [collapsed, setCollapsed] = useState(true)
 
 	return (
-		<header className="flex items-center justify-between flex-wrap bg-blue-300 text-black p-6 transition-all">
+		<header className="flex items-center justify-between flex-wrap bg-blue-300 text-black p-6 transition-all rounded-3xl m-4 shadow-lg">
 			<h1 className="text-xl font-extrabold w-full text-center sm:w-auto sm:text-left">
 				<HeaderLink title="msr.codes" href="/" />
 			</h1>
@@ -25,7 +29,7 @@ const Header: FC<HeaderProps> = ({links = []}) => {
 				<HeaderLinks links={links} />
 			</nav>
 			<button
-				className="border-black border p-2 absolute top-5 right-5 sm:hidden bg-white"
+				className="border-black border p-2 absolute top-9 right-9 sm:hidden bg-white"
 				type="button"
 				onClick={() => setCollapsed(!collapsed)}
 				aria-label="Toggle navigation"
