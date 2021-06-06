d8d381d065a5723074bf3341463437f560f73fb0
Mikael Rozee <mikaelsrozee@gmail.com>
2021-06-06 15:59:52 +0100

    enh(homepage): add content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import {NextPage} from 'next'
 import {DiTerminal} from 'react-icons/di'
 import {IoMdAnalytics} from 'react-icons/io'
+import {GiSpiderWeb} from 'react-icons/gi'
+
 import Card from '../components/Card'
 import ImageCard from '../components/Card/image'
 import Link from '../components/Link'
@@ -9,11 +11,14 @@ import Hero from '../components/Hero'
 const Page: NextPage = () => (
 	<>
 		{/* Header Hero  */}
-		<section className="h-[calc(100vh-4rem-3rem-1.5rem-4px)] prose text-center mx-auto flex px-8">
-			<div className="self-center items-center flex flex-col">
-				<h1>Full-Stack Software Engineer & Technical Analyst</h1>
-				<p className="text-lg">I code awesome things, and help everyone understand technology.</p>
-				<img className="w-64 h-64 rounded-full" src="https://placekitten.com/400/400" alt="" />
+		<section className="h-[calc(100vh-4rem-3rem-1.5rem-4px)] text-center mx-auto flex px-8">
+			<div className="self-center items-center flex flex-col mx-auto">
+				<div className="prose">
+					<h2>Hi, I&apos;m <span className="text-secondary">Mikael Rozee</span>.</h2>
+					<h1>Full-Stack Software Engineer & Technical Analyst</h1>
+					<p className="text-lg">I wear many hats. From solution architecture to delivery planning to hands-on-keyboard div slinging, I choose the right hats for any given task.</p>
+				</div>
+				<img className="w-32 h-32 md:w-64 md:h-64 mt-8 rounded-full" src="https://placekitten.com/400/400" alt="Hero" />
 			</div>
 		</section>
 		{/* About me */}
@@ -31,47 +36,96 @@ const Page: NextPage = () => (
 		</Hero>
 		{/* Skills & Experiences */}
 		<section className="px-8 z-0 relative -top-10">
-			<div className="mx-auto text-center flex flex-col md:flex-row space-y-8 md:space-y-0 max-w-5xl">
-				<Card>
+			<div className="mx-auto text-center grid grid-cols-1 md:grid-cols-3 gap-8 max-w-5xl">
+				<Card className="w-full">
 					<div className="prose">
-						<DiTerminal className="mx-auto w-20 h-20 text-primary-default" />
+						<GiSpiderWeb className="mx-auto w-20 h-20 text-primary-default" />
 						<h3>Web Developer</h3>
-						<p>I like to do web stuff, Lorem ipsum dolor sit.</p>
+						<p>
+							Web is my current focus. To me, the web is an accessible and diverse
+							platform that is exciting to be a part of.
+						</p>
 					</div>
-					<div>
+					<div className="mb-8">
 						<div className="prose mt-8">
-							<h4>In my toolbox:</h4>
+							<h4 className="!text-secondary">In my toolbox:</h4>
 						</div>
 						<ul>
-							<li>TypeScript (+ JavaScript, NodeJS)</li>
-							<li>Frameworks: React (Next.js, Gatsby) </li>
-							<li>CMS: Kentico Kontent, Contentful</li>
-							<li>RESTful APIs, GraphQL</li>
-							<li>TailwindCSS, SCSS</li>
-						</ul>
-					</div>
-					<div>
-						<div className="prose">
-							<h4>Dev Tools:</h4>
-						</div>
-						<ul>
-							<li>GitHub</li>
-							<li>Terminal</li>
-							<li>Vim</li>
-							<li>VSCode</li>
+							<li><strong>Languages:</strong> TypeScript (+ JavaScript, NodeJS)</li>
+							<li><strong>Frameworks:</strong> React (Next.js, Gatsby) </li>
+							<li><strong>CMS:</strong> Kentico Kontent, Contentful</li>
+							<li><strong>Database:</strong> PostgresSQL, NoSQL </li>
+							<li><strong>CI/CD:</strong> Azure DevOps, GitHub Actions</li>
+							<li><strong>APIs:</strong> RESTful APIs, GraphQL</li>
+							<li><strong>Style:</strong> TailwindCSS, SCSS</li>
+							<li><strong>Host:</strong> Microsoft Azure, Netlify</li>
+							<li className="prose"><Link href="/uses">...and more!</Link></li>
 						</ul>
 					</div>
 				</Card>
-				<Card>
+				<Card className="w-full">
+					<div className="prose">
+						<DiTerminal className="mx-auto w-20 h-20 text-primary-default" />
+						<h3>Software Engineer</h3>
+						<p>
+							Despite Web being my main focus now, I have a decade of experience in other
+							technologies.
+						</p>
+					</div>
+					<div>
+						<div className="prose mt-8">
+							<h4 className="!text-secondary">I have experience with:</h4>
+						</div>
+						<ul>
+							<li><strong>Java</strong> (+ Gradle, GSON)</li>
+							<li><strong>Python</strong> (+ pip, Selenium, TensorFlow)</li>
+							<li className="prose"><Link href="/uses">...and more!</Link></li>
+						</ul>
+					</div>
+					<div className="mb-8">
+						<div className="prose mt-8">
+							<h4 className="!text-secondary">With a little help from:</h4>
+						</div>
+						<ul>
+							<li><strong>GitHub</strong></li>
+							<li><strong>Terminal</strong> (Linux, WSL)</li>
+							<li><strong>Vim</strong> (primary editor)</li>
+							<li><strong>VSCode</strong> (secondary editor)</li>
+							<li><strong>IntelliJ IDEA</strong> (for Java, Python)</li>
+							<li><strong>Postman</strong></li>
+						</ul>
+					</div>
+				</Card>
+				<Card className="w-full">
 					<div className="prose">
 						<IoMdAnalytics className="mx-auto w-20 h-20 p-2 text-primary-default" />
 						<h3>Technical Analyst</h3>
-						<p>I like to deliver stuff, Lorem ipsum dolor sit.</p>
-						<h4>My experiences:</h4>
+						<p>Software Engineering is more than figuring out how to exit Vim.</p>
+						<p>
+							I enjoy working between technical and non-technical stakeholders to
+							elicit requirements and then build a solution with a team using the
+							right methodologies for the job.
+						</p>
 					</div>
-					<ul>
-						<li>Elanco Animal Health</li>
-					</ul>
+					<div className="mb-8">
+						<div className="prose mt-8">
+							<h4 className="!text-secondary">My Experiences:</h4>
+						</div>
+						<div className="prose">
+							<p><strong>Elanco Animal Health</strong>, Global Commercial IT</p>
+						</div>
+					</div>
+					<div className="mb-8">
+						<div className="prose mt-8">
+							<h4 className="!text-secondary">Analyst arsenal:</h4>
+						</div>
+						<ul>
+							<li><strong>Azure DevOps</strong> (Backlog management, reporting)</li>
+							<li><strong>Miro</strong> (Virtual whiteboarding)</li>
+							<li><strong>LucidChart</strong> (Solution design)</li>
+							<li><strong>EasyRetro</strong> (Sprint retrospectives)</li>
+						</ul>
+					</div>
 				</Card>
 			</div>
 		</section>
